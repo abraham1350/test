7e1ad2f6f422de1da3b312eae541f4036a74d922
abe <abrahamcelis@live.com>
2019-11-06 16:40:52 -0800

    Version 1.0

---
+++ create.c
@@ -0,0 +1,31 @@
+/*Just 
+ * testing
+ * stuff
+ * out
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+	printf("Yo, this is new to me.\n");
+
+	printf("What's 1 + 1?\n");
+
+	int dig;
+
+	scanf("%d", &dig);
+
+	if(dig == 2)
+	{
+		printf("That's right!");
+	}
+	else
+	{
+		printf("Really?\n NO.");
+	}
+
+}
